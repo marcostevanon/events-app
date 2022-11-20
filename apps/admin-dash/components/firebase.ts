57cf24e2efb3aec492fa0c94fe804f3eb2667224
marcostevanon <marco.stevanon@doctolib.com>
2022-11-20 14:33:18 +0100

    chore: setup firebase firestore

--- apps/admin-dash/components/firebase.ts
+++ apps/admin-dash/components/firebase.ts
@@ -1,19 +1,10 @@
-// Import the functions you need from the SDKs you need
+import { getAnalytics } from 'firebase/analytics';
 import { initializeApp } from 'firebase/app';
-// import { getAnalytics } from 'firebase/analytics';
-import {
-  GoogleAuthProvider,
-  getAuth,
-  signInWithPopup,
-  // signInWithEmailAndPassword,
-  // createUserWithEmailAndPassword,
-  // sendPasswordResetEmail,
-  // signOut,
-} from 'firebase/auth';
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: 'AIzaSyD0wsNwyrCBPZ5Wu9ijTzgslqbqlaZVwnE',
@@ -25,28 +16,18 @@ const firebaseConfig = {
   measurementId: 'G-B3PYE6L5B7',
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
+if (typeof window !== 'undefined') {
+  getAnalytics(app);
+}
 
+export const db = getFirestore(app);
 export const auth = getAuth(app);
 const googleProvider = new GoogleAuthProvider();
 
 export const signInWithGoogle = async () => {
   try {
-    const res = await signInWithPopup(auth, googleProvider);
-    const user = res.user;
-    console.log('signInWithGoogle ~ user', user);
-    // const q = query(collection(db, "users"), where("uid", "==", user.uid));
-    // const docs = await getDocs(q);
-    // if (docs.docs.length === 0) {
-    //   await addDoc(collection(db, "users"), {
-    //     uid: user.uid,
-    //     name: user.displayName,
-    //     authProvider: "google",
-    //     email: user.email,
-    //   });
-    // }
+    await signInWithPopup(auth, googleProvider);
   } catch (err) {
     console.error(err);
     alert(err.message);
--- apps/admin-dash/pages/index.tsx
+++ apps/admin-dash/pages/index.tsx
@@ -1,9 +1,15 @@
+import Link from 'next/link';
 import styled from 'styled-components';
 
 const StyledPage = styled.h1``;
 
 export function Index() {
-  return <StyledPage>Index!</StyledPage>;
+  return (
+    <StyledPage>
+      <div>Index!</div>
+      <Link href="/login">Go to login</Link>
+    </StyledPage>
+  );
 }
 
 export default Index;
