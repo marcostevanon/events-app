a35ef3d9cdc0849078f1ffd50b638af5d2d5a4e3
Marco Stevanon <marco.stevanon@doctolib.com>
2022-11-23 21:19:34 +0100

    fix: wrong layout container location

--- apps/admin-dashboard/src/app/layout/layout.tsx
+++ apps/admin-dashboard/src/app/layout/layout.tsx
@@ -47,31 +47,29 @@ function Layout(props: LayoutProps) {
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
-    <Container maxW="container.lg">
-      <Box minH="100vh" bg={useColorModeValue('gray.100', 'gray.900')}>
-        <SidebarContent
-          onClose={() => onClose}
-          display={{ base: 'none', md: 'block' }}
-        />
-        <Drawer
-          autoFocus={false}
-          isOpen={isOpen}
-          placement="left"
-          onClose={onClose}
-          returnFocusOnClose={false}
-          onOverlayClick={onClose}
-          size="full"
-        >
-          <DrawerContent>
-            <SidebarContent onClose={onClose} />
-          </DrawerContent>
-        </Drawer>
-        <MobileNav onOpen={onOpen} />
-        <Box ml={{ base: 0, md: 60 }} p="4">
-          {props.children}
-        </Box>
+    <Box minH="100vh" bg={useColorModeValue('gray.100', 'gray.900')}>
+      <SidebarContent
+        onClose={() => onClose}
+        display={{ base: 'none', md: 'block' }}
+      />
+      <Drawer
+        autoFocus={false}
+        isOpen={isOpen}
+        placement="left"
+        onClose={onClose}
+        returnFocusOnClose={false}
+        onOverlayClick={onClose}
+        size="full"
+      >
+        <DrawerContent>
+          <SidebarContent onClose={onClose} />
+        </DrawerContent>
+      </Drawer>
+      <MobileNav onOpen={onOpen} />
+      <Box ml={{ base: 0, md: 60 }} p="4">
+        <Container maxW="container.lg">{props.children}</Container>
       </Box>
-    </Container>
+    </Box>
   );
 }
 
